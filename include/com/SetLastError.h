52527a2d75e7bbe7967707f212c993a003e4d741
nick-crowley <nicholas.crowley@gmail.com>
2023-01-13 21:55:03 +0000

    refactor: Renamed `com::Exception` to `com::SetLastError`; moved to separate header; and extended to support constructor from narrow-char strings

---
+++ include/com/SetLastError.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "comFramework.h"
+#include "com/Guid.h"
+#include "com/CoClassTraits.h"
+#include "nstd/OutPtr.h"
+
+namespace core::com
+{
+	template <typename CoClass, typename Interface>
+	class SetLastError
+	{
+		using traits = coclass_traits<CoClass>;
+
+		::HRESULT m_result;
+
+	public:
+		SetLastError(::HRESULT result, std::string_view const msg) noexcept
+		  : SetLastError(result, std::wstring{msg.begin(), msg.end()})
+		{}
+
+		SetLastError(::HRESULT result, std::wstring_view const msg) noexcept
+		  : m_result(result)
+		{
+			shared_ptr<::ICreateErrorInfo> info;	
+			if (auto hr = ::CreateErrorInfo(nstd::out_ptr<::ICreateErrorInfo*>(info)); FAILED(hr))	// BUG: double-AddRef
+				this->m_result = hr;
+			else
+			{
+				info->SetDescription(const_cast<wchar_t*>(msg.data()));					// TODO: Check this works
+				info->SetSource(const_cast<wchar_t*>(traits::program_id.data()));
+				info->SetGUID(guid_v<Interface>);
+				
+				if (hr = ::SetErrorInfo(0, shared_ptr<::IErrorInfo>{info}); FAILED(hr))
+					this->m_result = hr;
+			}
+		}
+
+		implicit operator 
+		::HRESULT() const noexcept
+		{
+			return this->m_result;
+		}
+	};
+}
