129199c06887d09aa9551ef4d1d6864a1f662659
nick-crowley <nicholas.crowley@gmail.com>
2023-01-24 21:02:17 +0000

    refactor: Improved interface of `property()` methods of `com::adapter` so they return proxies, which are much more useful to derived classes

--- include/com/Adapter.h
+++ include/com/Adapter.h
@@ -12,24 +12,32 @@ namespace core::com
 	protected:
 		using interface_type = Interface;
 
+		template <typename ValueType>
+		using const_property_t = detail::ReadOnlyPropertyProxy<ValueType,interface_type>;
+
+		template <typename ValueType>
+		using mutable_property_t = detail::MutablePropertyProxy<ValueType,interface_type>; 
+
 	protected:
 		adapter(interface_type& obj) : m_object{obj}
 		{}
 
 	protected:
 		template <unsigned NumReturnParameters = 0, typename... Parameters>
-		auto method(::HRESULT (__stdcall Interface::*method)(Parameters...), auto... args) const {
+		auto
+		method(method_pointer_t<Interface,Parameters...> method, auto... args) const {
 			return com::method<NumReturnParameters>(method)(this->m_object, args...);
 		}
 
 		template <typename ValueType>
-		auto property(::HRESULT (__stdcall Interface::*get)(ValueType*)) const {
+		const_property_t<ValueType>
+		property(method_pointer_t<Interface,ValueType*> get) const {
 			return com::property(get)(this->m_object);
 		}
 		
 		template <typename ValueType>
-		auto property(::HRESULT (__stdcall Interface::*get)(ValueType*), 
-		              ::HRESULT (__stdcall Interface::*set)(ValueType)) const {
+		mutable_property_t<ValueType> 
+		property(method_pointer_t<Interface,ValueType*> get, method_pointer_t<Interface,ValueType> set) const {
 			return com::property(get, set)(this->m_object);
 		}
 	};
