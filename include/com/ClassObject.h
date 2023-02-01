536e20badefa82a047012a24e3f3aa91778e258e
nick-crowley <nicholas.crowley@gmail.com>
2023-02-01 23:55:30 +0000

    feat: Initial implementation of `com::ClassObject`

---
+++ include/com/ClassObject.h
@@ -0,0 +1,44 @@
+#pragma once
+#include "library/core.COM.h"
+#include "com/CoClassTraits.h"
+#include "win/RegistryKey.h"
+#include "com/Boolean.h"
+#include "com/Guid.h"
+#include "com/Function.h"
+#include "com/SharedPtr.h"
+
+namespace core::com
+{
+	template <meta::CoClass CoClass>
+	class ClassObject
+	{
+		auto constexpr
+		inline static coRegisterClassObject = function<1>(::CoRegisterClassObject);
+
+	private:
+		DWORD m_cookie;
+
+	public:
+		ClassObject(::IUnknown& instance, ::CLSCTX context, ::REGCLS usage)
+			: m_cookie{coRegisterClassObject(guid_v<CoClass>, &instance, context, usage)}
+		{}
+
+		~ClassObject() {
+			::CoRevokeClassObject(this->m_cookie);
+		}
+
+		satisfies(ClassObject,
+			NotDefaultConstructible,
+			NotCopyable,
+			NotMovable,
+			NotEqualityComparable,
+			NotSortable
+		);
+	};
+
+	void
+	inline resumeClassObjects() 
+	{
+		com::ThrowingHResult hr = ::CoResumeClassObjects();
+	}
+}
