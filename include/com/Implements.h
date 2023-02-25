ac09430af792adfa0d5c609a0bbbe9ad78d0ca9d
nick-crowley <nicholas.crowley@gmail.com>
2023-02-25 23:19:37 +0000

    feat: `com::implements<I...>` should only realize the set of distinct COM interfaces it was instantiated with to avoid repeated base classes and ambiguous conversions

--- include/com/Implements.h
+++ include/com/Implements.h
@@ -4,12 +4,93 @@
 #include "com/HResult.h"
 #include "core/FunctionLogging.h"
 
-namespace core::com 
+namespace core::com
 {
-	extern std::atomic_long g_numInstances;
+	namespace detail
+	{
+		//! @brief	Realizes an arbitrary type-list of COM interfaces
+		template <typename ForwardSequence>
+		class MultipleRealization; 
+
+		template <template<typename...> typename ForwardSequence, meta::Interface... Interfaces>
+		class MultipleRealization<ForwardSequence<Interfaces...>> : public Interfaces...
+		{
+			satisfies(MultipleRealization,
+				protected: IsDefaultConstructible noexcept,
+				public: IsPolymorphic,
+				NotCopyable,
+				NotEqualityComparable,
+				NotSortable
+			);
+		};
 	
+		//! @brief	Query whether @c Derived is a public and unambiguous descendant of @c Base (but is not @c Base)
+		template <meta::Interface Derived>
+		struct is_strict_base_of {
+			template <meta::Interface Base>
+			struct apply : std::conjunction<
+				std::is_convertible<Derived const*,Base const*>,
+				std::negation<std::is_same<Derived,Base>>
+			>
+			{};
+		};
+	
+		namespace testing 
+		{
+			using TestVector = mpl::vector<IClassFactory,IUnknown,ITypeInfo>;
+			using TestVector_WithoutIUnknown = typename mpl::remove_if<TestVector, is_strict_base_of<IClassFactory>>::type;
+			static_assert(mpl::size<TestVector_WithoutIUnknown>::value == 2);
+			static_assert(mpl::contains<TestVector_WithoutIUnknown,IClassFactory>::value);
+			static_assert(!mpl::contains<TestVector_WithoutIUnknown,IUnknown>::value);
+			static_assert(mpl::contains<TestVector_WithoutIUnknown,ITypeInfo>::value);
+
+			using TestVector2 = mpl::vector<IClassFactory2,IClassFactory,IDispatch>;
+			using TestVector2_WithoutIClassFactory = typename mpl::remove_if<TestVector2, is_strict_base_of<IClassFactory2>>::type;
+			static_assert(mpl::size<TestVector2_WithoutIClassFactory>::value == 2);
+			static_assert(mpl::contains<TestVector2_WithoutIClassFactory,IClassFactory2>::value);
+			static_assert(!mpl::contains<TestVector2_WithoutIClassFactory,IClassFactory>::value);
+			static_assert(mpl::contains<TestVector2_WithoutIClassFactory,IDispatch>::value);
+		}
+	
+		template <typename ForwardSequence>
+		struct distinct_interfaces_impl : std::type_identity_t<ForwardSequence>{};
+
+		template <typename I>
+		struct distinct_interfaces_impl<mpl::vector<I>> : std::type_identity_t<mpl::vector<I>>{};
+
+		template <typename I, typename ...R>
+		struct distinct_interfaces_impl<mpl::vector<I,R...>> : std::type_identity_t<
+			typename mpl::remove_if<mpl::vector<I,R...>,is_strict_base_of<I>>::type
+		>{};
+
+		template <meta::Interface... I>
+		struct distinct_interfaces : distinct_interfaces_impl<mpl::vector<I...>>
+		{};
+
+		//! @brief	Type-list of distinct COM interfaces (ie. with ancestral interfaces removed)
+		//! @tparam	Interfaces...	Set of COM interfaces, possibly including common base classes
+		template <meta::Interface... Interfaces>
+		using distinct_interfaces_t = typename distinct_interfaces<Interfaces...>::type;
+	
+		namespace testing
+		{
+			using TestVector3 = distinct_interfaces_t<IClassFactory,IUnknown>;
+			static_assert(mpl::size<TestVector3>::value == 1);
+			static_assert(mpl::contains<TestVector3,IClassFactory>::value);
+			static_assert(!mpl::contains<TestVector3,IUnknown>::value);
+
+			using TestVector4 = distinct_interfaces_t<IClassFactory,IDispatch,IUnknown>;
+			static_assert(mpl::size<TestVector4>::value == 2);
+			static_assert(mpl::contains<TestVector4,IClassFactory>::value);
+			static_assert(mpl::contains<TestVector4,IDispatch>::value);
+			static_assert(!mpl::contains<TestVector4,IUnknown>::value);
+		}
+	}
+
+	extern std::atomic_long g_numInstances;
+
 	template <meta::Interface... Interfaces>
-	class implements : public Interfaces...
+	class implements : public detail::MultipleRealization<detail::distinct_interfaces_t<Interfaces...>>
 	{	
 		using type = implements<Interfaces...>;
 
@@ -79,7 +160,16 @@ namespace core::com
 		{
 			if (__uuidof(Interface) == iid) 
 			{
-				*ppv = static_cast<Interface*>(this);
+				// Since coclasses commonly realize multiple interfaces, each of which inherits from IUnknown,
+				// it's highly likely there will not be an unambiguous conversion to IUnknown. COM also has an
+				// identity requirement stating that, whatever conversion we choose, it must be consistent; so
+				// we're converting via the first interface in the list.
+				if constexpr (!std::is_convertible_v<type const*, Interface const*>)
+					*ppv = static_cast<Interface*>(
+						static_cast<nstd::tuple_front_t<interface_tuple>*>(this)
+					);
+				else
+					*ppv = static_cast<Interface*>(this);
 				this->AddRef();
 				return S_OK;
 			}
--- src/libBoost.h
+++ src/libBoost.h
@@ -1,3 +1,9 @@
 #pragma once
 
 // NB: Add library-specific Boost dependencies here
+#include <boost/mpl/remove_if.hpp>
+#include <boost/mpl/contains.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/size.hpp>
+
+namespace mpl = boost::mpl;
