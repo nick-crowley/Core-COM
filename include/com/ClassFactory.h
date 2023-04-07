bdd52190c3e131b8380f510f6746cf24e8cf04c1
nick-crowley <nicholas.crowley@gmail.com>
2023-04-07 11:45:59 +0100

    docs: Added copyright and layout to `<com/ClassFactory.h>`

--- include/com/ClassFactory.h
+++ include/com/ClassFactory.h
@@ -1,42 +1,87 @@
+/* o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o */ /*!
+* @copyright	Copyright (c) 2023, Nick Crowley. All rights reserved.
+* 
+* Redistribution and use in source and binary forms, with or without modification, are permitted
+* provided that the following conditions are met:
+* 
+* 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+*    and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+*    conditions and the following disclaimer in the documentation and/or other materials provided
+*    with the distribution.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+* FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+* DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+* WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+* WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 
+* The views and conclusions contained in the software and documentation are those of the author 
+* and should not be interpreted as representing official policies, either expressed or implied, of
+* the projects which contain it.
+*/
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=o Preprocessor Directives o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 #pragma once
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Header Files o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 #include "library/core.COM.h"
 #include "com/Implements.h"
 #include "com/Guid.h"
 #include "com/HResult.h"
 #include "core/FunctionLogging.h"
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Name Imports o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
 
-namespace core::com::detail
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Forward Declarations o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Macro Definitions o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Constants & Enumerations o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Class Declarations o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+namespace core::com
 {
-	template <meta::CoImpl CoImpl, meta::ForwardSequence Interfaces>
-	struct CreateInstanceImpl {
-		::HRESULT
-		operator()(IUnknown*, ::IID const&, void**) const = delete; 
-	};
+	namespace detail
+	{
+		template <meta::CoImpl CoImpl, meta::ForwardSequence Interfaces>
+		struct CreateInstanceImpl {
+			::HRESULT
+			operator()(IUnknown*, ::IID const&, void**) const = delete; 
+		};
 
-	template <meta::CoImpl CoImpl, template<typename...> typename Sequence, meta::Interface... Interfaces>
-	struct CreateInstanceImpl<CoImpl,Sequence<Interfaces...>> {
-		::HRESULT
-		operator()(IUnknown*, ::IID const& iid, void** ppv) const
-		{
-			if ((... || (Guid(iid) == __uuidof(Interfaces))))
+		template <meta::CoImpl CoImpl, template<typename...> typename Sequence, meta::Interface... Interfaces>
+		struct CreateInstanceImpl<CoImpl,Sequence<Interfaces...>> {
+			::HRESULT
+			operator()(IUnknown*, ::IID const& iid, void** ppv) const
 			{
-				auto* inst = new CoImpl{};
-				return inst->QueryInterface(iid,ppv);
+				if ((... || (Guid(iid) == __uuidof(Interfaces))))
+				{
+					auto* inst = new CoImpl{};
+					return inst->QueryInterface(iid,ppv);
+				}
+				*ppv = nullptr;
+				return E_NOINTERFACE;
 			}
-			*ppv = nullptr;
-			return E_NOINTERFACE;
-		}
-	};
-}
+		};
+	}
 
-namespace core::com 
-{
+	/**
+	 * @brief	Co-class factory
+	 * 
+	 * @tparam	CoImpl	Type produced by factory
+	*/
 	template <meta::CoImpl CoImpl>
 		requires meta::ForwardSequence<typename CoImpl::interfaces>
 	class ClassFactory : public implements<::IClassFactory,::IUnknown>
 	{	
+		// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Types & Constants o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o
+	private:
 		using type = ClassFactory<CoImpl>;
-		
+
+		// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=o Representation o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o
+
+		// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Construction & Destruction o=~-~=~-~=~-~=~-~=~-~=~-~=~-~o
 	public:
 		satisfies(ClassFactory,
 			IsDefaultConstructible noexcept,
@@ -44,7 +89,13 @@ namespace core::com
 			NotEqualityComparable,
 			NotSortable
 		);
-		
+		// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o Copy & Move Semantics o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o
+
+		// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=o Static Methods o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o
+
+		// o~=~-~=~-~=~-~=~-~=~-~=~-~=~o Observer Methods & Operators o~-~=~-~=~-~=~-~=~-~=~-~=~-~o
+
+		// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Mutator Methods & Operators o~-~=~-~=~-~=~-~=~-~=~-~=~-~o
 	public:
 		::HRESULT
 		__stdcall CreateInstance(::IUnknown*, ::IID const& iid, void** ppv) override
@@ -78,3 +129,8 @@ namespace core::com
 		}
 	};
 }
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-o Non-member Methods o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~o Global Functions o~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
+
+// o~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=-o End of File o-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~o
