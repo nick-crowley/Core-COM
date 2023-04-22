849afe4080eab3c64bbd7e66188b9b68074bb55c
nick-crowley <nicholas.crowley@gmail.com>
2023-04-22 18:41:59 +0100

    fix: `Core-COM` should require client to define `_ATL_ATTRIBUTES`

--- include/library/core.COM.h
+++ include/library/core.COM.h
@@ -29,10 +29,6 @@
 #	define ComFramework_h_included
 #endif
 
-#ifndef _ATL_ATTRIBUTES
-	#define _ATL_ATTRIBUTES 
-#endif
-
 #ifdef _INC_WINDOWS
 #	error <Windows.h> has been included prior <core.COM.h>
 #endif
