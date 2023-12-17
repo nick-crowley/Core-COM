e69f66ba33c145b000d6a051782099e4cafac387
nick-crowley <nicholas.crowley@gmail.com>
2023-12-17 16:27:49 +0000

    fix: Directory `.\build\MIDL` must exist prior to first build

--- GenerateSolution.bat
+++ GenerateSolution.bat
@@ -3,6 +3,10 @@ echo Ensuring 'build' folder exists...
 mkdir .\build
 echo .
 
+echo Ensuring 'MIDL' sub-folder exists...
+mkdir .\build\MIDL
+echo .
+
 cd build
 cmake.exe -G "Visual Studio 17 2022" .\..
 cd ..
