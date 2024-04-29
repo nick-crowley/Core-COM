e207ffd42240bc33c50550a909592671ae3e08e8
nick-crowley <nicholas.crowley@gmail.com>
2024-04-29 18:14:46 +0100

    feat: Moved `boost` `mpl` support from _Core-COM_ project into _Core-Platform_ project

--- src/libBoost.h
+++ src/libBoost.h
@@ -1,22 +1,3 @@
 #pragma once
 
 // NB: Add library-specific Boost dependencies here
-#include <boost/mpl/remove_if.hpp>
-#include <boost/mpl/begin.hpp>
-#include <boost/mpl/end.hpp>
-#include <boost/mpl/contains.hpp>
-#include <boost/mpl/empty.hpp>
-#include <boost/mpl/front.hpp>
-#include <boost/mpl/vector.hpp>
-#include <boost/mpl/size.hpp>
-
-namespace mpl = boost::mpl;
-
-namespace boost::mpl
-{
-	template <typename Sequence>
-	using front_t = typename front<Sequence>::type;
-
-	template <typename Sequence, typename UnaryPredicate>
-	using remove_if_t = typename remove_if<Sequence,UnaryPredicate>::type;
-}
